a362706309a7be7ee375d63d34638ff445e0078a
lp-pyth <71586305+lp-pyth@users.noreply.github.com>
2020-12-04 16:45:03 +0100

    captureimagedemo
    take photo and show it, works

---
+++ CaptureImageDemo/app/src/main/java/com/example/captureimagedemo/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.captureimagedemo;
+
+//https://www.youtube.com/watch?v=RaOyw84625w
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    //Initialize Variable
+    ImageView imageView;
+    Button btOpen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Assign Variable
+        imageView = findViewById(R.id.image_view);
+        btOpen = findViewById(R.id.bt_open);
+
+        //Request for Camera Permission
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{
+                            Manifest.permission.CAMERA
+                    },
+                    100);
+        }
+        btOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Open Camera
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 100) {
+            //Get Capture Image
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            //Set Capture Image to ImageView
+            imageView.setImageBitmap(captureImage);
+        }
+    }
+}
